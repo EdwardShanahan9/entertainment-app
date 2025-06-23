fc82908f06484f7e6272c44a212f5a379a0af58a
EdwardShanahan9 <193254185+EdwardShanahan9@users.noreply.github.com>
2025-06-23 12:54:13 +0100

    Add font and color variables to tailwind.config file

--- src/layout/Dashboard/Dashboard.jsx
+++ src/layout/Dashboard/Dashboard.jsx
@@ -1,5 +1,7 @@
 const Dashboard = () => {
-  return <div className="text-3xl font-bold underline">Dashboard</div>;
+  return (
+    <div className="text-3xl text-primary font-bold underline">Dashboard</div>
+  );
 };
 
 export default Dashboard;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        outfit: ["Outfit", "sans-serif"],
+      },
+      colors: {
+        blue950: "#10141E",
+        blue900: "#161D2F",
+        blue500: "#5A698F",
+        primary: "#FC4747",
+      },
+    },
   },
   plugins: [],
 };
