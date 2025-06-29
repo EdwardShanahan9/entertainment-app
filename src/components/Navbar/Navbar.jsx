8ea1ef7a46a382695c9e3ad77b988a7c0f810281
EdwardShanahan9 <193254185+EdwardShanahan9@users.noreply.github.com>
2025-06-29 13:54:46 +0100

    Style Navbar component for mobile devices screen sizes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,49 @@
+import LogoIcon from "../../assets/logo.svg";
+import HomeIcon from "../../assets/icon-nav-home.svg";
+import MoviesIcon from "../../assets/icon-nav-movies.svg";
+import TvIcon from "../../assets/icon-nav-tv-series.svg";
+import BookmarkIcon from "../../assets/icon-nav-bookmark.svg";
+import AvaterIcon from "../../assets/image-avatar.png";
+
 const Navbar = () => {
   return (
-    <nav>
-      <h1>Navbar</h1>
+    <nav className="bg-blue-900 p-4 flex items-center justify-between">
+      <img className="w-[25px] h-[20px]" src={LogoIcon} alt="Logo" />
+
+      <ul className="flex items-center">
+        <li>
+          <a href="#">
+            <img className="w-4 h-4 mr-6" src={HomeIcon} alt="Home Icon" />
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <img className="w-4 h-4 mr-6" src={MoviesIcon} alt="Movies Icon" />
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <img className="w-4 h-4 mr-6" src={TvIcon} alt="Tv Series Icon" />
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <img
+              className="w-4 h-4 mr-6"
+              src={BookmarkIcon}
+              alt="Bookmark Series Icon"
+            />
+          </a>
+        </li>
+      </ul>
+
+      <a href="">
+        <img
+          className="w-6 rounded-full border-white border-2"
+          src={AvaterIcon}
+          alt="Avatar Icon"
+        />
+      </a>
     </nav>
   );
 };
--- src/layout/Dashboard/Dashboard.jsx
+++ src/layout/Dashboard/Dashboard.jsx
@@ -2,7 +2,7 @@ import Navbar from "../../components/Navbar/Navbar";
 
 const Dashboard = () => {
   return (
-    <div className="px-4 md:p-6 lg:p-8">
+    <div className="md:p-6 lg:p-8">
       <Navbar />
     </div>
   );
